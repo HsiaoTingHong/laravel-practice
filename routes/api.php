63248c26c3ed85f19b5f1bd6453bba59456347cc
Ting <hsiaoting1003@gmail.com>
2025-04-04 18:23:33 +0800

    設定路由

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\TypeController; // 下方設定路由時這邊會自動新增
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // apiResource 為 laravel 內建的方法，將路由網址設定為 animals 字串，對應到 AnimalController 內相對應的方法
 Route::apiResource('animals', AnimalController::class);
+Route::apiResource('types', TypeController::class);
