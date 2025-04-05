7514b33fbfc52ae0c4d487f934f3adac5443b738
Ting <hsiaoting1003@gmail.com>
2025-04-05 12:19:15 +0800

    Controller & Model

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,17 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Type;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\Response;
 
 class TypeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. 查詢資料列表
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        // 分類資料量少直接全部輸出
+        $types = Type::get();
+
+        return response([
+            'data' => $types // 輸出使用 data 包住
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -28,25 +35,52 @@ class TypeController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage. 新增資料
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        // 驗證資料
+        $this->validate($request, [
+            // 另一種驗證的寫法，使用陣列傳入驗證關鍵字
+            'name' => [
+                'required',
+                'max:50',
+                // type 資料表中的 name 欄位資料是唯一值
+                Rule::unique('types', 'name')
+            ],
+            'sort' => 'nullable|integer',
+        ]);
+
+        // 如果沒有傳入 sort 欄位內容
+        if (!isset($request->sort)) {
+            // 找到目前資料表的排序欄位最大值
+            $max = Type::max('sort');
+            // 最大值加一寫入請求的資料中
+            $request['sort'] = $max + 1;
+        }
+
+        // 寫入資料庫
+        $type = Type::create($request->all());
+
+        return response([
+            'data' => $type
+        ], Response::HTTP_CREATED);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource. 查詢單一資料
      *
      * @param  \App\Models\Type  $type
      * @return \Illuminate\Http\Response
      */
     public function show(Type $type)
     {
-        //
+        return response([
+            'data' => $type
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -61,7 +95,7 @@ class TypeController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified resource in storage. 修改資料
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Type  $type
@@ -69,17 +103,35 @@ class TypeController extends Controller
      */
     public function update(Request $request, Type $type)
     {
-        //
+        // 驗證資料
+        $this->validate($request, [
+            'name' => [
+                'max:50',
+                // 更新時排除自己的名稱後，檢查是否為唯一值
+                Rule::unique('types', 'name')->ignore($type->name, 'name')
+            ],
+            'sort' => 'nullable|integer',
+        ]);
+
+        // 更新資料
+        $type->update($request->all());
+
+        return response([
+            'data' => $type
+        ], Response::HTTP_OK);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource from storage. 刪除資料
      *
      * @param  \App\Models\Type  $type
      * @return \Illuminate\Http\Response
      */
     public function destroy(Type $type)
     {
-        //
+        // 把這個實體物件刪除
+        $type->delete();
+        // 回傳 null 並且給予 204 狀態碼
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    use HasFactory;
+    /**
+     * 可以被批量賦值的屬性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'sort'
+    ];
 }
