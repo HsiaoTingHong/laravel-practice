2c4985ddc0cc5b80fea8a495eeb10a6f08be71d8
Ting <hsiaoting1003@gmail.com>
2025-04-05 12:55:56 +0800

    建立模型關聯 一對多

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -30,4 +30,14 @@ class Animal extends Model
     // 為什麼要設定 $fillable? 因為像AnimalController 這樣的撰寫方式，把整個使用者請求的陣列直接用create 的方法寫入，會有安全性的問題，所以必須用 $fillable來限制哪些欄位可以被批量寫入。
 
     // 假設今天是User被創建的動作，如果沒有這一層的保護，使用者的請求如果有包含權限的欄位值，就可以把User設定成管理員的權限。
+
+    /**
+     * 取得 Animal 的分類
+     */
+    public function type()
+    {
+        // Animal 的 Model 也要加上反向的關聯，這樣就可以從 Animal 這個方向去查詢 type
+        // belongsTo(類別名稱，參照欄位，主鍵)
+        return $this->belongsTo('App\Models\Type');
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -15,4 +15,14 @@ class Type extends Model
         'name',
         'sort'
     ];
+    /**
+     * 取得 Type 的動物
+     */
+    public function animals()
+    {
+        // 在 Type 的 Model 中加入與 Animal 的一對多關聯
+        // 建立一個方法名為 animals()，實做呼叫自身 Model 類別的 hasMany() 方法來設定類別
+        // hasMany(類別名稱，參照欄位，主鍵)
+        return $this->hasMany('App\Models\Animal', 'type_id', 'id');
+    }
 }
