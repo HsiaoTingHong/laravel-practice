c0cbc33587ba5b77174fe75ea7e10d4d631fa6a2
Ting <hsiaoting1003@gmail.com>
2025-04-19 14:29:52 +0800

    身份驗證 - 設定 Passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        // Undefined method 'routes'.intelephense(P1013) - 上網查這個方法已經沒有了，Passport 會自動設定好路由
+        // Passport::routes();
+        // Passport::ignoreRoutes();
 
         //
     }
