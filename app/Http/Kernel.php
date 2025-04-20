ef309d5ef6f17a4da877214449fd6b0cd4a5af3c
Ting <hsiaoting1003@gmail.com>
2025-04-20 19:46:05 +0800

    註冊中介層的三種方法：全域註冊，群組註冊，路由註冊

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
+// 註冊中介層的三種方法：全域註冊，群組註冊，路由註冊
 class Kernel extends HttpKernel
 {
     /**
@@ -13,6 +14,7 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+    // 全域註冊
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
@@ -28,7 +30,9 @@ class Kernel extends HttpKernel
      *
      * @var array<string, array<int, class-string|string>>
      */
+    // 群組註冊
     protected $middlewareGroups = [
+        // routes\web.php 檔案中設定的路由套用這個群組的中介層
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -38,6 +42,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        // routes\api.php 檔案中設定的路由套用這個群組的中介層
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
@@ -52,7 +57,9 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
+    // 路由註冊
     protected $routeMiddleware = [
+        // 定義一個名詞呼叫 Middleware 用於指派給路由使用
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
